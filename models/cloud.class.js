e3d29793a159ecd26ac3cef54314e13509960342
ILONA KOSCHI <ILONA.KOSCHI@HOTMAIL.com>
2024-04-22 09:24:43 +0200

    animated character and chickens

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,5 +1,5 @@
 class Cloud extends MovableObject {
-    y = 20;
+    y = 0;
     width = 600;
     height = 450;
 
@@ -7,6 +7,13 @@ class Cloud extends MovableObject {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
 
         this.x = Math.random() * 500;
+        this.animate();
+    }
+
+    animate(){
+        setInterval(() => {
+            this.x -= 0.2;
+        }, 1000 / 30);
     }
 
 
